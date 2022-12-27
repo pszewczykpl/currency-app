838d1f0574776b7be6642fa81d8ba80cb92902ab
pszewczykpl <pszewczykpl@gmail.com>
2022-12-27 21:35:13 +0100

    Przygotowano model Currency wraz z obsługą UUID

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -3,9 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
 
 class Currency extends Model
 {
     use HasFactory;
+
+    /**
+     * We're using UUIDs as primary keys.
+     */
+    use HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'currency_code',
+        'exchange_rate',
+    ];
 }
