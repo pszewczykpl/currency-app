2f592e37bb89f3f2060d47d7d302be689d426f5e
pszewczykpl <pszewczykpl@gmail.com>
2022-12-27 22:47:28 +0100

    Usunięto niepotrzebne elementy z projektu

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// No API routes for this project.
